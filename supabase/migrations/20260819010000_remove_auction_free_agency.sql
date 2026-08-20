e38e6afa13d4a5fe49b7f9d88165634143f08a35
carloshernandezave256-dotcom <carloshernandezave256@gmail.com>
2026-08-19 19:48:57 -0700

    Remove auction free agency

---
+++ supabase/migrations/20260819010000_remove_auction_free_agency.sql
@@ -0,0 +1,7 @@
+-- Auction leagues use blind contract offers exclusively. There is no
+-- first-come free-agency period after offers are processed.
+revoke all on function public.sign_available_player(uuid,bigint,bigint)
+from public,anon,authenticated;
+
+drop function public.sign_available_player(uuid,bigint,bigint);
+
